cd376fde20b1bff0e6b3d18f76b8f5e60e6341c3
MuhammadRayhanTriPutra <muhammadrayhantri@student.esaunggul.ac.id>
2026-04-09 16:19:40 +0700

    09-04-2026

---
+++ pert1-variable_printing/variable_printing.py
@@ -0,0 +1,21 @@
+NAMA="Muhammad Rayhan Tri Putra";
+NIM=20250801080;
+
+print("Nama :", NAMA)
+print("NIM  :", NIM)
+
+Nama= input("Masukan Nama anda: ");
+NIM= int(input("Masukan NIM anda: "));
+Jurusan= input("Masukan Jurusan anda: ");
+tempatTglLahir= input("Masukan Tempat/Tgl-Lahir anda: ");
+Alamat= input("Alamat tinggal / domisili anda: ");
+
+# print(f"Haloooo {NAMA}, Selamat Belajar Bahasa Pemrograman!");
+# print(f"\nNama: {Nama}\nNIM: {NIM}\nJurusan: {Jurusan}\nTempat&Tgl-Lahir: {tempatTglLahir}\nAlamat Tinggal: {Alamat}");
+
+print("\n");
+print(f"Nama              :", Nama)
+print(f"NIM               :", NIM)
+print(f"Jurusan           :", Jurusan)
+print(f"Tempat / Tgl-Lahir:", tempatTglLahir)
+print(f"Alamat            :", Alamat)

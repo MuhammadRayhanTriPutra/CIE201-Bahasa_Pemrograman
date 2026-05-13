b199e1e7f3203ddc6de16e083cbfeddc33176515
MuhammadRayhanTriPutra <muhammadrayhantri@student.esaunggul.ac.id>
2026-05-13 20:26:46 +0700

    16-04-2026

---
+++ pert2-data_type/example.py
@@ -0,0 +1,19 @@
+angka = 25;
+nilai = 3.14;
+nama = "Teknik Informatika";
+status = True;
+
+print(angka);
+print(nilai);
+print(nama);
+print(status);
+
+angka = 25;
+nilai = 3.14;
+nama = "Python";
+status = True;
+
+print(type(angka))
+print(type(nilai))
+print(type(nama))
+print(type(status))
